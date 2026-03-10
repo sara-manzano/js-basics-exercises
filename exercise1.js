41ecafbcf54d42edf8cb2633682a02a4b951e0dc
Sara <sara@quesu.local>
2026-03-10 13:06:37 +0100

    Initial commit: Add JavaScript exercise files

---
+++ exercise1.js
@@ -0,0 +1,12 @@
+let myFavoriteHero = "Hulk";
+let x = 50;
+let h = 5;
+let y = 10;
+let z = h + y;
+
+console.log("Hero: ", myFavoriteHero);
+console.log("Valor de x: ", x);
+console.log("Valor de h: ", h);
+console.log("Valor de y: ", y);
+console.log("Valor de z: ", h + y);
+
---
+++ exercise3.js
@@ -0,0 +1,37 @@
+let x = 10;
+let y = 5;
+
+console.log(x * y);
+
+
+let a = 10;
+let b = 2;
+
+console.log(a / b);
+
+
+let h = 15;
+let z = 9;
+
+console.log(h % z);
+
+
+let p = 10;
+let j = 5;
+let o = p;
+
+o += j;
+
+console.log(o);
+
+
+let c = 10;
+let m = 5;
+let i = c;
+
+i *= m;
+
+console.log(i);
+
+
+
